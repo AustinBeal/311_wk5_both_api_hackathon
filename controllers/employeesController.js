4cc7d265a56e8e54a7331fc8f1f920c4e968f358
Cody-Blair <cb.rialbc@gmail.com>
2021-01-17 13:43:10 -0600

    Set up dynamic controllers for employees table

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -2,12 +2,25 @@ const mysql = require('mysql')
 const pool = require('../mysql/connections')
 
 const getUsers = (req,res) => {
-    res.send("Getting Employess")
+    let sql = 'SELECT * FROM EMPLOYEES LIMIT 50'
+    sql = mysql.format(sql)
+
+    pool.query(sql, (err, rows) => {
+        if (err) return res.status(500).send("something went wrong")
+        return res.json(rows)
+    })
 }
 
 const getUsersById = (req,res) => {
     let id = req.params.id
-    res.send(`employee number ${id}`)
+    let sql = `SELECT * FROM EMPLOYEES WHERE emp_no = '${id}'`
+    sql = mysql.format(sql)
+
+    pool.query(sql, (err, rows) => {
+        if (err) return res.status(500).send("something went wrong")
+        if (id < 10001) return res.send('Numbers start from 10001')
+        return res.json(rows)
+    })
 }
 
 const getUserByFirstname = (req,res) => {
